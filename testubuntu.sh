5e97bb2699200be511852b2ec4a0dc07163e6590
samaliftech <31126508+samaliftech@users.noreply.github.com>
2021-01-30 23:02:27 +0800

    Update testubuntu.sh

--- testubuntu.sh
+++ testubuntu.sh
@@ -1,3 +1,4 @@
 pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
 ./start-ubuntu.sh
 apt update && upgrade
+apt install nano -y
